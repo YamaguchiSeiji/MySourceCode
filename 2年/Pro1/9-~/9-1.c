452e40c66741415c9e621985417105cee6c434a9
YamaguchiSeiji <s1230s122@gmail.com>
2025-03-13 02:09:02 +0900

    Delete 2年/Pro1 directory

--- 2年/Pro1/9-~/9-1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-	int num;
-	printf("整数を10進数で入力してください:");
-	scanf("%d",&num);
-	printf("8進数:%o\n",num);
-	printf("16進数:%X\n",num);
-	return 0;
-}
--- 2年/Pro1/summer_work/sw1-1.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(){
-	int d,l;
-	scanf("%d %d",&d,&l);
-	printf("%d\n",d/l+d%l);
-	return 0;
-}
